bef735190c5f5f38cce86ae9280c5ece294737bc
Suyadi <suyadi.1992@gmail.com>
2019-10-26 12:51:22 +0700

    :mask: remove ability to load plugins

--- system/core/Model.php
+++ system/core/Model.php
@@ -4,17 +4,10 @@ defined('BASE') or exit('No direct script access allowed');
 
 class Model extends \Loader
 {
-    public $db;
-    protected $load;
+    protected $db;
 
-    /**
-     * Constructor.
-     */
     public function __construct()
     {
-        $this->load = parent::getInstance();
-        $env = $this->load->config('config');
-        $config = $this->load->config('database');
-        $this->db = $this->load->database();
+        $this->db = parent::getInstance()->database();
     }
 }
